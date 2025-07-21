2770baaeafe7c90a93333fcda424819d3c317e4d
Sankalp Singh <74290353+1vishen@users.noreply.github.com>
2025-07-21 06:13:55 +0000

    lc, easy, 3

---
+++ src/main/java/coding-problems/Length of Last Word.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int lengthOfLastWord(String s) {
+        int count = 0;
+        int i = s.length() - 1;
+
+        while(i >= 0 && s.charAt(i) == ' '){
+            i--;
+        }
+
+        for(; i >= 0; i--){
+            if(s.charAt(i) == ' ') break;
+            else count++;
+        }
+        return count;
+    }
+}
