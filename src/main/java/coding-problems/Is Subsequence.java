1b8c82889f1830b1469e30ca14ef1e0cdf1b4145
Sankalp Singh <74290353+1vishen@users.noreply.github.com>
2025-07-30 18:18:48 +0000

    lc, easy 1

---
+++ src/main/java/coding-problems/Is Subsequence.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+        int sPointer = 0;
+        for(int i = 0; i < t.length(); i++) {
+            if(sPointer < s.length() && s.charAt(sPointer) == t.charAt(i)) {
+                sPointer++;
+            }
+        }
+        return sPointer == s.length();
+    }
+}
