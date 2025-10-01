5879f9450ddc02ab65bc3e0b7c40f9b547cb7284
Sankalp Singh <74290353+1vishen@users.noreply.github.com>
2025-10-01 08:35:34 +0000

    lc, med, 1

--- src/main/java/coding-problems/Set Matrix Zeroes.java
+++ src/main/java/coding-problems/Set Matrix Zeroes.java
@@ -1,39 +1,72 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
-        // store all original locations where zeros are
-        List<Integer> rowIndexArr =  new ArrayList<>();
-        List<Integer> colIndexArr = new ArrayList<>();
+        /* for a solution without extra space, we use the 0th row and 0th column 
+        as markers to indicate which rows and columns should be zeroed. */
 
-        for(int i = 0; i < matrix.length; i++) {
-            for(int j = 0; j < matrix[0].length; j++) {
-                if(matrix[i][j] == 0) {
-                    rowIndexArr.add(i);
-                    colIndexArr.add(j);
+        /* However, there's a critical caveat: if we don't use separate flags to track 
+        whether the first row or first column originally contained a zero, we risk 
+        misinterpreting the markers. For example, if matrix[0][j] is set to 0 due to 
+        a zero found in a lower row, we can't tell whether that zero was originally 
+        present in the first row or was placed there as a marker. As a result, we 
+        might mistakenly zero out the entire first row or column even if it wasn't 
+        supposed to be. */
+
+        int rowLength = matrix.length;
+        int colLength = matrix[0].length;
+        boolean firstRowHasZero = false;
+        boolean firstColHasZero = false;
+
+        for (int j = 0; j < colLength; j++) {
+            if (matrix[0][j] == 0)
+                firstRowHasZero = true;
+        }
+        for (int i = 0; i < rowLength; i++) {
+            if (matrix[i][0] == 0)
+                firstColHasZero = true;
+        }
+        /* make 0th row and col zero if there's any zero in [i][j], 
+            starting from 1st row and col, leaving 0th row and col */
+        for (int i = 1; i < rowLength; i++) {
+            for (int j = 1; j < colLength; j++) {
+                if (matrix[i][j] == 0) {
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
+                }
+
+            }
+        }
+
+        /* set all elements in entire row as zero if element in 0th col is zero, starting from
+         1st row*/
+        for (int i = 1; i < rowLength; i++) {
+            int j = 0;
+            if (matrix[i][0] == 0) {
+                while (j < colLength) {
+                    matrix[i][j++] = 0;
+                }
+            }
+        }
+        /* set all elements in entire col as zero if element in 0th row is zero, starting from
+         1st col */
+        for (int j = 1; j < colLength; j++) {
+            int i = 0;
+            if (matrix[0][j] == 0) {
+                while (i < rowLength) {
+                    matrix[i++][j] = 0;
                 }
             }
         }
 
-        // now using those original locations make entire row/col zero
-        for(int i : rowIndexArr) {
-            setRowZero(i, matrix);
+        // check if both  boolean are true to also make all of 0th row & col as zero
+        if (firstColHasZero) {
+            for (int i = 0; i < rowLength; i++) {
+                matrix[i][0] = 0;
+            }
         }
-        for(int j : colIndexArr) {
-            setColZero(j, matrix);
-        }
-    }
-
-    private void setColZero(int element, int[][] matrix) {
-        int lengthOfRow = matrix.length;
-        for(int i = 0; i < lengthOfRow; i++) {
-            matrix[i][element] = 0;
-        }
-    }
-
-    private void setRowZero(int element, int[][] matrix) {
-        // assuming all col are of same length
-        int lengthOfCol = matrix[0].length;
-        for(int i = 0; i < lengthOfCol; i++) {
-            matrix[element][i] = 0;
+        if (firstRowHasZero) {
+            for (int j = 0; j < colLength; j++) {
+                matrix[0][j] = 0;
+            }
         }
     }
 }
