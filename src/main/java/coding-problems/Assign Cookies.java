429dbdba35d9636a1c2d5c5bd41de1898489bb43
Sankalp Singh <74290353+1vishen@users.noreply.github.com>
2025-11-07 10:45:27 +0000

    lc, easy, 1

---
+++ src/main/java/coding-problems/Assign Cookies.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+        /* Skip cookies, not children, to find the smallest cookie that satisfies each child's greed cause, If a cookie is too small it can't help move to the next one. */
+        Arrays.sort(g);
+        Arrays.sort(s);
+
+        int i = 0, j = 0;
+
+        while (i < g.length && j < s.length) {
+            if (s[j] >= g[i]) i++;
+            j++;
+        }
+        return i;
+
+    }
+}
